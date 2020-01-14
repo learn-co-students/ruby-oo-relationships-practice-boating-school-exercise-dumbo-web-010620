2e758cbf6a9d09274af1c1809d5991736de237e3
Jin Wu <jinn.wuu@gmail.com>
2020-01-14 16:50:13 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,36 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, status, instructor)
+        BoatingTest.new(self, test_name, status, instructor)
+    end
+
+    def self.find_student(student_name)
+        all.find{ |i| i.first_name == student_name}
+    end
+
+    def grade_percentage
+        student_test = BoatingTest.all.select{|i|i.student == self}
+        test = student_test.count.to_f
+        student_pass = student_test.select{|i|i.status == "passed"}
+        pass = student_pass.count.to_f
+        percent = (pass/test)*100
+    end
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+student1 = Student.new("Apple")
+student2 = Student.new("Banana")
+
+instructor1 = Instructor.new("Instructor_1")
+instructor2 = Instructor.new("Instructor_2")
+
+test1 = BoatingTest.new(student1, "Test 1", "passed", instructor1)
+test2 = BoatingTest.new(student2, "Test 2", "pending", instructor1)
+test3 = BoatingTest.new(student2, "Test 1", "pending", instructor2)
+test4 = BoatingTest.new(student1, "Test 3", "pending", instructor1)
+test5 = BoatingTest.new(student1, "Test 4", "pending", instructor2)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
